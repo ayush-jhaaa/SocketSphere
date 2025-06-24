012f25c5f5c87727132f4ddebac21640e326e54e
sugar-daddyy <aj747274@gmail.com>
2025-06-25 02:51:58 +0530

    user schema updated

--- pepper/src/models/User.ts
+++ pepper/src/models/User.ts
@@ -5,6 +5,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required : true,
+    unique : true,
+  },
+  password: {
+    type: String,
+    required : true,
+  }
 });
 
 //ek naya schema banaya h
